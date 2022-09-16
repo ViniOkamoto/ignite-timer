24d8a0a6891fc77a9989831eb2e04a73d34b5b88
viniciusokamoto <leo_kamoto@hotmail.com>
2022-09-15 19:04:19 -0500

    feat(Eslint): apply eslit extension in code

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,27 +1,26 @@
-import styled, {css} from 'styled-components';
+import styled, { css } from 'styled-components'
 
-
-export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
 interface ButtonProps {
-    variant: ButtonVariant;
- };
+  variant: ButtonVariant
+}
 const buttonVariants = {
-    primary: 'purple',
-    secondary: 'orange',
-    danger: 'red',
-    success: 'green',
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green',
 }
 export const ButtonContainer = styled.button<ButtonProps>`
-    width: 100px;
-    height: 50px;
-    border: 0;
-    border-radius: 8px;
-    margin: 8px;
+  width: 100px;
+  height: 50px;
+  border: 0;
+  border-radius: 8px;
+  margin: 8px;
 
-    background-color: ${props => props.theme.primary};
-    color: ${props => props.theme.white};
-    /* ${props => css`
-    background-color: ${buttonVariants[props.variant]}
-    `} */
-`;
+  background-color: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme.white};
+  /* ${(props) => css`
+    background-color: ${buttonVariants[props.variant]};
+  `} */
+`
