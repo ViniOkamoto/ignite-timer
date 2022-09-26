8ff9f4f189f6ad8ff22ba0756d8c79ad4a4c9b9f
viniciusokamoto <leo_kamoto@hotmail.com>
2022-09-25 19:57:37 -0500

    feat(CountdownContext): add countdown logic in a context

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import { GlobalStyle } from './styles/global'
 import Router from './Router'
 import { BrowserRouter } from 'react-router-dom'
 import CyclesContextProvider from './contexts/CycleContext'
+import CountdownContextProvider from './contexts/CountdownContext'
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
         <CyclesContextProvider>
-          <Router />
+          <CountdownContextProvider>
+            <Router />
+          </CountdownContextProvider>
           <GlobalStyle />
         </CyclesContextProvider>
       </ThemeProvider>
--- src/pages/home/components/countdown/index.tsx
+++ src/pages/home/components/countdown/index.tsx
@@ -1,59 +1,11 @@
 import { differenceInSeconds } from 'date-fns'
 import { useContext, useEffect } from 'react'
+import { CountdownContext } from '../../../../contexts/CountdownContext'
 import { CyclesContext } from '../../../../contexts/CycleContext'
 import { CountdownContainer, CountDownDivider } from './styles'
 
 export default function Countdown() {
-  const {
-    activeCycle,
-    activeCycleId,
-    markCycleAsFinished,
-    amountSecondPassed,
-    setSecondsPassed,
-  } = useContext(CyclesContext)
-
-  const totalSeconds = activeCycle ? activeCycle.taskDuration * 60 : 0
-  const currentSeconds = activeCycle ? totalSeconds - amountSecondPassed : 0
-  const minutesAmount = Math.floor(currentSeconds / 60)
-  const secondsAmount = currentSeconds % 60
-  const minutes = String(minutesAmount).padStart(2, '0')
-  const seconds = String(secondsAmount).padStart(2, '0')
-
-  useEffect(() => {
-    let interval: number
-    if (activeCycle) {
-      interval = setInterval(() => {
-        const secondsDiff = differenceInSeconds(
-          new Date(),
-          new Date(activeCycle.startedAt),
-        )
-
-        if (secondsDiff >= totalSeconds) {
-          markCycleAsFinished()
-          setSecondsPassed(totalSeconds)
-          clearInterval(interval)
-        } else {
-          setSecondsPassed(secondsDiff)
-        }
-      }, 1000)
-    }
-    return () => {
-      clearInterval(interval)
-    }
-  }, [
-    activeCycle,
-    activeCycleId,
-    markCycleAsFinished,
-    setSecondsPassed,
-    totalSeconds,
-  ])
-
-  useEffect(() => {
-    if (activeCycle) {
-      document.title = `Running ${minutes}:${seconds}`
-    } else document.title = `Ignite Timer`
-  }, [minutes, seconds, activeCycle])
-
+  const { minutes, seconds } = useContext(CountdownContext)
   return (
     <CountdownContainer>
       <span>{minutes[0]}</span>
